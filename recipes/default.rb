4947313b69f32e7e465a8b41e9806499f50334b0
Dion Hutchings <dion.hutchings@inrix.com>
2014-06-25 13:30:07 -0700

    Attributes not populated

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,17 +6,18 @@ apt_package "logstash" do
 	action :install
 end
 
+curl_cmd = "curl -O --user '#{node['logstash']['config_file_gist']['userid']}:#{node['logstash']['config_file_gist']['pwd']}' '#{node['logstash']['config_file_gist']['download_url']}'"
+
 bash 'config gist' do
 	cwd "/etc/logstash/conf.d"
 	code <<-EOH
-	curl -O --user '#{node['logstash']['config_file_gist']['userid']}:#{node['logstash']['config_file_gist']['pwd']}' '#{node['logstash']['config_file_gist']['download_url']}'
+	#{curl_cmd}
 	tar -zxf download
 	cd gist*
 	mv gistfile1.txt ../logstash.conf
 	rm -rf gist*
 	rm download
 	EOH
-  	command ""
 end
 
 service "logstash" do
